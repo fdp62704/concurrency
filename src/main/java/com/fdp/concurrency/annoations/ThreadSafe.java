50d130f9f2770281b9b2505d1952b37c7f4d0381
Aaron <1510840542@qq.com>
2018-07-06 16:30:35 +0800

    + 新增标识注解类

--- src/main/java/com/fdp/concurrency/annoations/ThreadSafe.java
+++ src/main/java/com/fdp/concurrency/annoations/ThreadSafe.java
@@ -1,7 +1,16 @@
 package com.fdp.concurrency.annoations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by yan on 2018/7/6.
+ * 课程里面用来标记【线程安全】的类或者写法
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface ThreadSafe {
+    String value() default "";
 }
